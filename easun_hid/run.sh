f81ee828244d4bea7326584b11981b698a7a4b40
dodoldzzz-lab <dodoldzzz@gmail.com>
2025-12-12 23:49:51 +0100

    Update run.sh

--- easun_hid/run.sh
+++ easun_hid/run.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 set -e
+
 echo "Starting EASUN HID Reader add-on..."
-python3 /app/easun_hid.py
+
+export HID_DEVICE=/dev/hidraw0
+export MQTT_HOST=localhost
+export MQTT_PORT=1883
+export MQTT_PREFIX=easun1
+
+exec python3 /app/easun_hid.py
+
